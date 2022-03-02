5d1b45c99ca9d4e1297d730bc65efed4a4f8b811
Reshma <70480663+ReshmaBabuChethan@users.noreply.github.com>
2022-03-02 10:58:52 -0600

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnqkzkKAdYAWfiWZLgiUtezI7_cTgIflg",
+    authDomain: "reshcapstone.firebaseapp.com",
+    projectId: "reshcapstone",
+    storageBucket: "reshcapstone.appspot.com",
+    messagingSenderId: "572410599196",
+    appId: "1:572410599196:web:5a85776c78fffd6b328cd9"
+  }
+
+  // init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
