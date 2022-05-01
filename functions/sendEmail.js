609c1315deedbbdd7d9753b16e090c1badbf6094
Reshma <70480663+ReshmaBabuChethan@users.noreply.github.com>
2022-05-01 14:50:51 -0500

    to the point where a email is sent

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,7 +1,32 @@
-const {https} = require("firebase-functions");
+const { https } = require('firebase-functions');
+const { createTransport } = require('nodemailer');
 
-const sendEmail = https.onRequest((req, res) => {
-    res.send({status: 200});
+const sender = 'resh.capstone@gmail.com';
+const password = '';
+const recipient = 'reshmabjm82@gmail.com';
+
+const transporter = createTransport({
+  service: 'gmail',
+  auth: {
+    user: sender,
+    pass: password,
+  },
 });
 
-module.exports = sendEmail;
+const mailOptions = {
+  from: sender,
+  to: recipient,
+  subject: 'Firebase Message',
+  text: 'Working',
+  html: '<h1>Working</h1>',
+};
+
+const transport = (error, { messageId }) =>
+  error ? console.log(error) : console.log(messageId);
+
+const handleEmail = (req, res) => {
+  transporter.sendMail(mailOptions, transport);
+  res.send({ status: 200 });
+};
+
+module.exports = https.onRequest(handleEmail);
