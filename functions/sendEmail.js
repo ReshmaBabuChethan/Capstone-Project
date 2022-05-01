cbfc96d7036bfaaeaf953a1d0f29f8babffe7f3c
Reshma <70480663+ReshmaBabuChethan@users.noreply.github.com>
2022-05-01 15:19:23 -0500

    to the point where a cors is added

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,32 +1,49 @@
-const { https } = require('firebase-functions');
-const { createTransport } = require('nodemailer');
+const {https} = require('firebase-functions');
+const {createTransport} = require('nodemailer');
 
-const sender = 'resh.capstone@gmail.com';
-const password = '';
+
+const cors = require('cors');
+const corsHandler = cors({origin: true});
+
+const {
+    gmail: {
+        password,
+        sender
+    }
+} = config();
+
 const recipient = 'reshmabjm82@gmail.com';
 
 const transporter = createTransport({
-  service: 'gmail',
-  auth: {
-    user: sender,
-    pass: password,
-  },
+    service: 'gmail',
+    auth: {
+        user: sender,
+        pass: password
+    }
 });
 
 const mailOptions = {
-  from: sender,
-  to: recipient,
-  subject: 'Firebase Message',
-  text: 'Working',
-  html: '<h1>Working</h1>',
+    from: sender,
+    to: recipient,
+    subject: 'Firebase Message',
+    text: 'Working',
+    html: '<h1>Working</h1>'
 };
 
-const transport = (error, { messageId }) =>
-  error ? console.log(error) : console.log(messageId);
+const transport = (error, {messageId}) => error ? console.log(error) : console.log(messageId);
+
+// const handleEmail = (req, res) => {
+// transporter.sendMail(mailOptions, transport);
+// res.send({ status: 200 });
+// };
+
+// module.exports = https.onRequest(handleEmail);
 
 const handleEmail = (req, res) => {
-  transporter.sendMail(mailOptions, transport);
-  res.send({ status: 200 });
+    corsHandler(req, res, () => {
+        transporter.sendMail(mailOptions, transport);
+    });
+    res.send({status: 200});
 };
 
 module.exports = https.onRequest(handleEmail);
